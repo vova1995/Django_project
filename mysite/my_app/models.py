cd9e9a3ef4cef4149b1b019c529e18c82f4dc130
vova1995 <vovapetryna1995@gmail.com>
2018-03-29 18:49:01 +0300

    added UserProfile class

--- mysite/my_app/models.py
+++ mysite/my_app/models.py
@@ -2,4 +2,6 @@ from django.db import models
 
 # Create your models here.
 class UserProfile(models.Model):
-	pass
+	name = models.CharField(max_length=150, default='')
+	surname = models.CharField(max_length=150, default='')
+	email = models.EmailField()
