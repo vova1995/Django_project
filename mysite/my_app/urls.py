2364f7ff6a293078526dc5a958cc4eb5a1277870
vova1995 <vovapetryna1995@gmail.com>
2018-03-29 18:30:08 +0300

    adding login page and redirect to home

--- mysite/my_app/urls.py
+++ mysite/my_app/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
 from . import views
+from django.contrib.auth.views import login
 
 
 urlpatterns = [
 	url(r'^home', views.home, name='home'),
-
+	url(r'^login/$', login, {'template_name': 'my_app/login.html'}),
 ]
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -123,6 +123,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+LOGIN_REDIRECT_URL = '/home/'
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
