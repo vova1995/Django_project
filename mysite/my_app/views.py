154001a572f6d3ae414dedf174747a835d54b1bd
vova1995 <vovapetryna1995@gmail.com>
2018-03-29 17:38:32 +0300

    changed home.html and added link to catalog

--- mysite/my_app/views.py
+++ mysite/my_app/views.py
@@ -6,8 +6,12 @@ from datetime import date
 def home(request):
 	
 	# return HttpResponse('<h1>Hello' + name + '</h1>')
+
 	context = {
 		# 'currentdate': date,
-		'catalog': Catalog.objects.all(),
+
+		'catalog': Catalog.objects.all()[:8],
+
 	}
 	return render(request, 'my_app/home.html', context)
+
