42743a40d390f4109abb9487d348cf103662f6ac
vova1995 <vovapetryna1995@gmail.com>
2018-03-30 19:38:19 +0300

    chaged user profile

--- mysite/my_app/models.py
+++ mysite/my_app/models.py
@@ -1,7 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+
 
 # Create your models here.
 class UserProfile(models.Model):
+	user = models.OneToOneField(User)
 	name = models.CharField(max_length=150, default='')
 	surname = models.CharField(max_length=150, default='')
 	email = models.EmailField()
+
+	def __str__(self):
+		return 'User: ' + str(self.user) + ', ' + 'Surname:  '+ self.surname + ', ' + 'Email: ' + self.email
+		# return str(self.user) + '\t' + self.surname + '\t' + self.email
+
+def create_profile(sender, **kwargs):
+	if kwargs['created']:
+		user_profile = UserProfile.objects.create(user=kwargs['instance'])
+
+post_save.connect(create_profile, sender=User)
