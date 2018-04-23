6a21347c38296a86860b86b7c24377be17a5c5da
vova1995 <vovapetryna1995@gmail.com>
2018-04-23 18:04:55 +0300

    changed home.html

--- mysite/my_app/admin.py
+++ mysite/my_app/admin.py
@@ -2,4 +2,4 @@ from django.contrib import admin
 from my_app.models import UserProfile
 
 # Register your models here.
-admin.site.register(UserProfile)
+admin.site.register(UserProfile)#register model from models in django admni
